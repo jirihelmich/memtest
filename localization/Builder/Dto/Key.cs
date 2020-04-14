f7e4e62da9cb750a2b7a918e2ab177a1dfc09be9
skurik <standa.kurik@gmail.com>
2020-04-14 20:09:09 +0200

    Add DTOs

---
+++ localization/Builder/Dto/Key.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Mews.LocalizationBuilder.Dto
+{
+    public sealed class Key
+    {
+        public KeyScopes Scopes { get; set; }
+
+        public IEnumerable<string> Parameters { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
---
+++ localization/Builder/Dto/VersionData.cs
@@ -0,0 +1,9 @@
+﻿namespace Mews.LocalizationBuilder.Dto
+{
+    public sealed class VersionData
+    {
+        public string Version { get; set; }
+
+        public string Commit { get; set; }
+    }
+}
