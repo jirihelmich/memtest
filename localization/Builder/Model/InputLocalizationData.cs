dae8de9de575024380e6971d580bf238ee258e20
skurik <standa.kurik@gmail.com>
2020-04-14 14:16:38 +0200

    [TDE-3179] Added local data models to localization builder.

---
+++ localization/Builder/Model/InputLocalizationData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Mews.LocalizationBuilder.Model
+{
+    public sealed class InputLocalizationData
+    {
+        private InputLocalizationData(IDictionary<string, Translation> data)
+        {
+            Data = new Dictionary<string, Translation>(data);
+        }
+
+        public Dictionary<string, Translation> Data { get; }
+
+        public static InputLocalizationData Read(string valuePath, string sourceLanguage)
+        {
+            var languageDirectories = GetLanguageDirectories(valuePath);
+            var data = languageDirectories.GroupBy(d => d.Language).ToDictionary(
+                g => g.Key,
+                g => ReadLanguageData(g.SelectMany(t => Directory.GetFiles(t.Path, "*.resjson")), includeMetadata: g.Key.SafeEquals(sourceLanguage))
+            );
+
+            return new InputLocalizationData(data);
+        }
+
+        private static Translation ReadLanguageData(IEnumerable<string> filePaths, bool includeMetadata)
+        {
+            var languageData = filePaths.Select(p => Translation.FromJson(File.ReadAllText(p), includeMetadata: includeMetadata));
+            var combined = languageData.Aggregate(Translation.Combine);
+
+            return combined;
+        }
+
+        private static IEnumerable<(string Language, string Path)> GetLanguageDirectories(string basePath)
+        {
+            var directories = Directory.GetDirectories(basePath).Select(p => (FullPath: p, DirectoryName: Path.GetFileName(p)));
+            return directories.Where(d => Regex.IsMatch(d.DirectoryName, "[a-z]{2}_[A-Z]{2}")).Select(d => (
+                Language: d.DirectoryName.Replace("_", "-"),
+                Path: d.FullPath
+            ));
+        }
+    }
+}
---
+++ localization/Builder/Model/Translation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using FuncSharp;
+using Newtonsoft.Json.Linq;
+
+namespace Mews.LocalizationBuilder.Model
+{
+    public sealed class Translation
+    {
+        private Translation(IDictionary<string, Key> data)
+        {
+            Data = new Dictionary<string, Key>(data);
+        }
+
+        public Dictionary<string, Key> Data { get; }
+
+        private static Regex KeyNameRegex => new Regex(@"^_?([a-zA-Z0-9]+)\.?", RegexOptions.Compiled);
+
+        public static Translation Combine(Translation first, Translation second)
+        {
+            var combinedData = first.Data.Concat(second.Data).ToDictionary(p => p.Key, p => p.Value);
+            return new Translation(combinedData);
+        }
+
+        public static Translation FromJson(string json, bool includeMetadata)
+        {
+            var jsonDocument = JObject.Parse(json);
+            var keys = GetKeys(jsonDocument, includeMetadata);
+            var data = keys.ToDictionary(key => key.Name);
+
+            return new Translation(data);
+        }
+
+        private static IStrictEnumerable<Key> GetKeys(IDictionary<string, JToken> jsonDocument, bool includeMetadata)
+        {
+            var keyNames = jsonDocument.Keys.Distinct(KeyName);
+            var keys = keyNames.Select(name => CreateKey(name, jsonDocument, includeMetadata));
+
+            return StrictEnumerable.Create(keys);
+        }
+
+        private static Key CreateKey(string name, IDictionary<string, JToken> json, bool includeMetadata)
+        {
+            var text = json[name].Value<string>();
+            var cleanText = text.Replace('\n', ' ').Trim();
+            var metaKey = $"_{name}{Key.MetaSuffix}";
+            var metadata = includeMetadata.ToTrueOption().FlatMap(_ => json.GetOrElse(metaKey).MapRef(t => t.ToObject<KeyMetadata>()).ToOption());
+
+            return new Key(
+                name,
+                cleanText,
+                metadata.GetOrNull()
+            );
+        }
+
+        private static string KeyName(string jsonKey)
+        {
+            return KeyNameRegex.Match(jsonKey).Groups[1].Value;
+        }
+    }
+}
