2150d521424717a15a6a06cb55193dddc37f48ce
skurik <standa.kurik@gmail.com>
2020-04-14 20:20:19 +0200

    [TDE-3133] Added a guard for translation key removal.

---
+++ localization/Builder/Git/GitClient.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mews.LocalizationBuilder.Extensions;
+using Octokit;
+
+namespace Mews.LocalizationBuilder.Git
+{
+    public sealed class GitClient
+    {
+        public GitClient(string repositoryOwner, string repository, string accessToken)
+        {
+            RepositoryOwner = repositoryOwner;
+            Repository = repository;
+            Client = new GitHubClient(new ProductHeaderValue("devops-pipeline"));
+            Client.Credentials = new Credentials(accessToken);
+        }
+
+        private string RepositoryOwner { get; }
+
+        private string Repository { get; }
+
+        private GitHubClient Client { get; }
+
+        public GitHubCommit GetCommit(string commitHash)
+        {
+            return Client.Repository.Commit.Get(RepositoryOwner, Repository, commitHash).Result();
+        }
+
+        public IReadOnlyList<GitHubCommit> GetCommits(string firstCommitHash, string lastCommitHash)
+        {
+            var comparisonResult = Client.Repository.Commit.Compare(RepositoryOwner, Repository, firstCommitHash, lastCommitHash).Result();
+            return comparisonResult.Commits.Select(c => GetCommit(c.Sha)).ToList();
+        }
+    }
+}
---
+++ localization/Builder/Helper/Git.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using FuncSharp;
+
+namespace Mews.LocalizationBuilder.Helper
+{
+    public static class Git
+    {
+        private static Regex KeyChangePattern => new Regex(@"^(-|\+)\s*""([a-zA-Z]+)""");
+
+        public static IEnumerable<string> GetDeletions(string diffPatch)
+        {
+            return GetKeyModifications(diffPatch).Where(m => m.Change.Equals(KeyChange.Deletion)).Select(m => m.Key);
+        }
+
+        private static IEnumerable<(string Key, KeyChange Change)> GetKeyModifications(string diffPatch)
+        {
+            var modifications = diffPatch.Split(Environment.NewLine).Select(line =>
+            {
+                var match = KeyChangePattern.Match(line);
+                return match.Success.Match(
+                    t => (
+                        Key: match.Groups[2].Value,
+                        Change: match.Groups[1].Value.Match(
+                            "-", _ => KeyChange.Deletion,
+                            "+", _ => KeyChange.Addition
+                        )
+                    ).ToOption(),
+                    f => Option.Empty<(string Key, KeyChange Change)>()
+                );
+            }).Flatten();
+
+            return modifications.GroupBy(m => m.Key).Select(g => (
+                Key: g.Key,
+                Change: GetActualChange(g.Select(m => m.Change))
+            ));
+        }
+
+        private static KeyChange GetActualChange(IEnumerable<KeyChange> changes)
+        {
+            if (changes.Contains(KeyChange.Addition) && changes.Contains(KeyChange.Deletion))
+            {
+                return KeyChange.Modification;
+            }
+
+            return changes.Single();
+        }
+    }
+}
