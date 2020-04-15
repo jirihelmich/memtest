1023d87f29e079bfd2b2aa267a59e0d4ebd89ba4
skurik <standa.kurik@gmail.com>
2020-04-15 16:08:13 +0200

    WIP

--- localization/Builder/Program.cs
+++ localization/Builder/Program.cs
@@ -2,10 +2,6 @@
 using System.Linq;
 using CommandLine;
 using FuncSharp;
-using Mews.LocalizationBuilder.Model;
-using Mews.LocalizationBuilder.Storage;
-using Mews.LocalizationBuilder.Validation;
-using Mews.Time;
 
 namespace Mews.LocalizationBuilder
 {
@@ -22,39 +18,7 @@ namespace Mews.LocalizationBuilder
 
         private static ITry<Unit, IStrictEnumerable<Validation.Error>> Run(Options options)
         {
-            var storageClient = new StorageClient(
-                containerUri: new Uri(options.StorageContainerUri),
-                tenantId: options.ServicePrincipalTenantId,
-                clientId: options.ServicePrincipalClientId,
-                clientSecret: options.ServicePrincipalClientSecret
-            );
-
-            var version = GetFreshVersion();
-            var localData = InputLocalizationData.Read(options.DataDirectory, options.SourceLanguage);
-            var currentData = storageClient.ReadCurrentVersion();
-            var errors = currentData.FlatMap(data => Validator.Validate(localData, data, options.Commit, options.SourceLanguage).AsNonEmpty());
-
-            return errors.Match(
-                Try.Error<Unit, IStrictEnumerable<Validation.Error>>,
-                _ =>
-                {
-                    var updatedData = new VersionedLocalizationData(
-                        versionData: new VersionData(version, options.Commit),
-                        localization: localData.Serialize(options.SourceLanguage)
-                    );
-
-                    storageClient.Upload(updatedData);
-                    storageClient.Update(new Manifest(version, version));
-
-                    return Try.Success<Unit, IStrictEnumerable<Validation.Error>>(Unit.Value);
-                }
-            );
-        }
-
-        private static Version GetFreshVersion()
-        {
-            var now = StaticDateTimeProvider.NowUtc;
-            return new Version($"{now.Year}{now.Month:D2}{now.Day:D2}.{now.Hour}.{now.Minute}.{now.Second}");
+            return Try.Success<Unit, IStrictEnumerable<Validation.Error>>(Unit.Value);
         }
     }
 }
