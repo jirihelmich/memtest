7a42a5e1501c3ef7bdfefd24a751355c1395a4f4
skurik <standa.kurik@gmail.com>
2020-04-15 16:14:49 +0200

    Add storage client and the publishing itself.

--- localization/Builder/Program.cs
+++ localization/Builder/Program.cs
@@ -2,6 +2,10 @@
 using System.Linq;
 using CommandLine;
 using FuncSharp;
+using Mews.LocalizationBuilder.Model;
+using Mews.LocalizationBuilder.Storage;
+using Mews.LocalizationBuilder.Validation;
+using Mews.Time;
 
 namespace Mews.LocalizationBuilder
 {
@@ -18,7 +22,39 @@ namespace Mews.LocalizationBuilder
 
         private static ITry<Unit, IStrictEnumerable<Validation.Error>> Run(Options options)
         {
-            return Try.Success<Unit, IStrictEnumerable<Validation.Error>>(Unit.Value);
+            var storageClient = new StorageClient(
+                containerUri: new Uri(options.StorageContainerUri),
+                tenantId: options.ServicePrincipalTenantId,
+                clientId: options.ServicePrincipalClientId,
+                clientSecret: options.ServicePrincipalClientSecret
+            );
+
+            var version = GetFreshVersion();
+            var localData = InputLocalizationData.Read(options.DataDirectory, options.SourceLanguage);
+            var currentData = storageClient.ReadCurrentVersion();
+            var errors = currentData.FlatMap(data => Validator.Validate(localData, data, options.Commit, options.SourceLanguage).AsNonEmpty());
+
+            return errors.Match(
+                Try.Error<Unit, IStrictEnumerable<Validation.Error>>,
+                _ =>
+                {
+                    var updatedData = new VersionedLocalizationData(
+                        versionData: new VersionData(version, options.Commit),
+                        localization: localData.Serialize(options.SourceLanguage)
+                    );
+
+                    storageClient.Upload(updatedData);
+                    storageClient.Update(new Manifest(version, version));
+
+                    return Try.Success<Unit, IStrictEnumerable<Validation.Error>>(Unit.Value);
+                }
+            );
+        }
+
+        private static Version GetFreshVersion()
+        {
+            var now = StaticDateTimeProvider.NowUtc;
+            return new Version($"{now.Year}{now.Month:D2}{now.Day:D2}.{now.Hour}.{now.Minute}.{now.Second}");
         }
     }
 }
---
+++ localization/Builder/Storage/Manifest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Mews.LocalizationBuilder.Storage
+{
+    public sealed class Manifest
+    {
+        public Manifest(Version latest, Version current)
+        {
+            Latest = latest;
+            Current = current;
+        }
+
+        public Version Latest { get; }
+
+        public Version Current { get; }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(new
+            {
+                Latest = Latest.ToString(),
+                Current = Current.ToString()
+            }, Formatting.Indented);
+        }
+    }
+}
